ddb2129e492eacfa4081052e284ec39678ff02d3
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-19 22:28:52 +0300

    created singleton class

--- src/Untestables.mjs
+++ src/Untestables.mjs
@@ -1,3 +1,23 @@
 export function now() {
-  return new Date().getTime();
+    return new Date().getTime();
+}
+
+export class Singleton {
+    id;
+
+    constructor(id) {
+        if (Singleton._instance) {
+            throw new Error("Singleton classes can't be instantiated more than once.")
+        }
+        Singleton._instance = this;
+        this.id = id;
+    }
+
+    static get instance() {
+        return Singleton._instance;
+    }
+
+    getId() {
+        return this.id;
+    }
 }
