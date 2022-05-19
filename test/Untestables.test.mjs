bcdf4fed7d62e8b7702bd6c1d5ccbc7a2eb1f16f
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-19 22:32:19 +0300

    created test for singleton

--- test/Untestables.test.mjs
+++ test/Untestables.test.mjs
@@ -1,8 +1,19 @@
 import { expect } from "chai";
 import { now } from "../src/Untestables.mjs";
+import { Singleton } from "../src/Untestables.mjs";
 
-describe("Time based test", () => {
+describe("Time test", () => {
+    // This test won't pass all the time, but randomly.
     it("get current time", () => {
         expect(now()).to.equal(new Date().getTime());
+    });
+});
+
+describe("Singleton test", () => {
+    // This test won't pass because I can't instantiate the class more than once.
+    it("get id of two singleton instances", () => {
+        const instance1 = new Singleton("1");
+        const instance2 = new Singleton("1");
+        expect(instance1.getId()).to.equal(instance2.getId());
     });
 });
